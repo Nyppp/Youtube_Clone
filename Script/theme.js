59425996afa6d5e89e6851cd540ab799b91b039d
bangjjun <p01085488742@gmail.com>
2025-05-08 17:09:27 +0900

    create theme
    라이트 모드 구현

--- Script/theme.js
+++ Script/theme.js
@@ -1,19 +1,27 @@
-const toggleBtn = document.getElementById('toggle-theme');
-const body = document.body;
+let toggleBtn;
+
+//토글 버튼이 유효할 때 까지 반복
+const interval = setInterval(()=>{
+  if(toggleBtn){
+    // 버튼 클릭 시 테마 적용
+    toggleBtn.addEventListener("click", () => {
+      body.classList.toggle("light-mode");
+
+      // 현재 상태 저장
+      if (body.classList.contains("light-mode")) {
+        localStorage.setItem("theme", "light");
+      } else {
+        localStorage.setItem("theme", "dark");
+      }
+    });
+      clearInterval(interval);
+  }
+  toggleBtn = document.getElementById('toggle-theme');
+  const body = document.body;
+}, 100);
+
 
 // 페이지 로드 시 localStorage에서 테마 상태 불러오기
 if (localStorage.getItem("theme") === "light") {
     body.classList.add("light-mode");
 }
-
-// 버튼 클릭 시 테마 적용
-toggleBtn.addEventListener("click", () => {
-    body.classList.toggle("light-mode");
-
-    // 현재 상태 저장
-  if (body.classList.contains("light-mode")) {
-    localStorage.setItem("theme", "light");
-  } else {
-    localStorage.setItem("theme", "dark");
-  }
-});
